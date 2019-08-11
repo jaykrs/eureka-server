08daee33792e66ae2386bfe2c88baa7fb8e7dd2d
Jayant Kumar <jaykrs@gmail.com>
2019-08-12 01:16:00 +0530

    Add files via upload

---
+++ articlecms/src/main/java/com/article/articlecms/controller/DemoController.java
@@ -0,0 +1,20 @@
+package com.article.articlecms.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+	@Autowired
+	  private Environment environment;
+	
+	@RequestMapping(value = "/hallo/{name}", method = RequestMethod.GET)
+	public String sayhi(@PathVariable String name) {
+		return "Hallo <h2> " + name+ "</h1>";
+	}
+}
