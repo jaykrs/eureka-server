6a5023674d3ba9d5eb3184de4d3dbbae862d7fab
Jayant Kumar <jaykrs@gmail.com>
2019-08-15 12:59:00 +0530

    alpha

--- articlecms/src/main/java/com/article/articlecms/ijcr/IJcrAuthentication.java
+++ articlecms/src/main/java/com/article/articlecms/ijcr/IJcrAuthentication.java
@@ -2,9 +2,10 @@ package com.article.articlecms.ijcr;
 
 import javax.jcr.LoginException;
 import javax.jcr.RepositoryException;
+import javax.jcr.SimpleCredentials;
 
 public interface IJcrAuthentication {
 
 	public boolean loginRepository(String userId, String pwd) throws LoginException, RepositoryException;
-	
+	public boolean createRepository(SimpleCredentials simpleCredential ,String repositoryName);
 }
--- articlecms/src/main/java/com/article/articlecms/jcrimpl/JcrAuthenticationImpl.java
+++ articlecms/src/main/java/com/article/articlecms/jcrimpl/JcrAuthenticationImpl.java
@@ -1,10 +1,17 @@
 package com.article.articlecms.jcrimpl;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import javax.jcr.AccessDeniedException;
 import javax.jcr.GuestCredentials;
 import javax.jcr.LoginException;
 import javax.jcr.Repository;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
+import javax.jcr.SimpleCredentials;
+import javax.jcr.UnsupportedRepositoryOperationException;
 
 import org.apache.jackrabbit.commons.JcrUtils;
 import org.apache.logging.log4j.util.Strings;
@@ -13,7 +20,6 @@ import com.article.articlecms.ijcr.IJcrAuthentication;
 
 public class JcrAuthenticationImpl implements IJcrAuthentication{
 
-	@Override
 	public boolean loginRepository(String userId, String pwd) throws LoginException, RepositoryException {
 		Repository repository = JcrUtils.getRepository();
         Session session = repository.login(new GuestCredentials());
@@ -26,6 +32,48 @@ public class JcrAuthenticationImpl implements IJcrAuthentication{
             session.logout(); 
         }
         return false;
+	}
+
+	public static void main(String s[]) throws LoginException, RepositoryException {
+		System.out.println(new JcrAuthenticationImpl().loginRepository("", ""));
+		System.out.println("repository "+new JcrAuthenticationImpl().createRepository("products"));
+	}
+	@Override
+	public boolean createRepository(String repositoryName) {
+		// TODO Auto-generated method stub
+		 Repository repo = null  ;
+		 Session session = null;
+		try {
+			 repo = JcrUtils.getRepository();
+			session = repo.login(new SimpleCredentials("admin", "admin".toCharArray()));
+		} catch (LoginException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		} catch (RepositoryException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		}
+		 try {
+			 repo.login(new SimpleCredentials("admin", "admin".toCharArray()), repositoryName);
+		} catch (RepositoryException e) {
+			try {
+				session.getWorkspace().createWorkspace(repositoryName);
+			} catch (AccessDeniedException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (UnsupportedRepositoryOperationException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (RepositoryException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			e.printStackTrace();
+		}
+		 
+		
+		 
+		return true;
 	}
 
 }
